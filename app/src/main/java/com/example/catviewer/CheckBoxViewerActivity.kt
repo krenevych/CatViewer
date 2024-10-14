1ab2b45fc0df6705481ddc09ad958aa873e8b84f
Andrii Krenevych <krenevych@knu.ua>
2024-10-14 11:23:28 +0300

    added CheckBoxViewerActivity

--- app/src/main/java/com/example/catviewer/CheckBoxViewerActivity.kt
+++ app/src/main/java/com/example/catviewer/CheckBoxViewerActivity.kt
@@ -1,15 +1,59 @@
 package com.example.catviewer
 
 import android.os.Bundle
+import android.widget.CompoundButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.catviewer.databinding.ActivityCheckBoxViewerBinding
 
-class CheckBoxViewerActivity : AppCompatActivity() {
+class CheckBoxViewerActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
+
+    private lateinit var binding: ActivityCheckBoxViewerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_check_box_viewer)
+        binding = ActivityCheckBoxViewerBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.checkBoxRelaxed.setOnCheckedChangeListener(this)
+        binding.checkBoxSitting.setOnCheckedChangeListener(this)
+        binding.checkBoxJumping.setOnCheckedChangeListener(this)
+
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+
+        if (!isChecked){
+            return
+        }
+
+        val res = when (buttonView) {
+            binding.checkBoxRelaxed -> {
+//                binding.checkBoxRelaxed.isChecked = false
+                binding.checkBoxSitting.isChecked = false
+                binding.checkBoxJumping.isChecked = false
+                R.drawable.cat_relaxed
+            }
+            binding.checkBoxSitting -> {
+                binding.checkBoxRelaxed.isChecked = false
+//                binding.checkBoxSitting.isChecked = false
+                binding.checkBoxJumping.isChecked = false
+                R.drawable.cat_sitting
+            }
+            binding.checkBoxJumping -> {
+                binding.checkBoxRelaxed.isChecked = false
+                binding.checkBoxSitting.isChecked = false
+//                binding.checkBoxJumping.isChecked = false
+                R.drawable.cat_jumping
+            }
+            else -> {R.drawable.cat_relaxed}
+        }
+
+        binding.imageViewCat.setImageResource(res)
+
     }
 }
